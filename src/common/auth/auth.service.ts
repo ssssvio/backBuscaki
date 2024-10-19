219f394ddd15443c47d97236fa16c7596aca838b
ssssvio <r.savio@proton.me>
2024-10-19 10:55:00 -0300

    Refactor auth service to use FindMerchantService for user lookup

--- src/common/auth/auth.service.ts
+++ src/common/auth/auth.service.ts
@@ -1,28 +1,31 @@
+import * as bcryptjs from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDTO } from './dto/login-dto';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-
+import { FindMerchantService } from '../finders/find-merchant.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly jwtService: JwtService,
-    private readonly findUserService: FindUsersService,
+    private readonly findUsernameService: FindMerchantService,
   ) { }
 
-  async login(login: LoginDto) {
-    const { email, password } = login;
+  async login(login: LoginDTO) {
+    const { username, password } = login;
 
-    const user = await this.findUserService.findOneByEmail(email);
+    const user = await this.findUsernameService.findByUsername(username);
     if (!user || !(await bcryptjs.compare(password, user.password))) {
       throw new UnauthorizedException('Invalid credentials!');
     };
 
-    const payload = { email: user.email, sub: user.id };
+    const payload = { username: user.username, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
   };
 
   async validateUser(payload: any): Promise<any> {
-    return { userId: payload.sub, email: payload.email };
+    return { userId: payload.sub, username: payload.username };
   };
 };
