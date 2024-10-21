e0dea47bc76b41ee0c57e21b516acd0eabc46a61
ssssvio <r.savio@proton.me>
2024-10-21 14:54:57 -0300

    Refactor create-merchant.interface.ts

--- src/modules/merchant/interfaces/create-merchant.interface.ts
+++ src/modules/merchant/interfaces/create-merchant.interface.ts
@@ -1,7 +1,6 @@
 import { MerchantEntity } from "src/data/merchant";
 import { MerchantDTO } from "../dto/merchant-dto";
 
-
 export interface ICreateMerchantUsecase {
   execute(data: MerchantDTO): Promise<MerchantEntity>;
 };
