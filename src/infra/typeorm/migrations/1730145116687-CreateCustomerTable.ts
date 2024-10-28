8fc79681b6b802acde6413f4959d7115a032e904
ssssvio <r.savio@proton.me>
2024-10-28 16:56:32 -0300

    Refactor merchant table creation migration

--- src/infra/typeorm/migrations/1730145116687-CreateCustomerTable.ts
+++ src/infra/typeorm/migrations/1730145116687-CreateCustomerTable.ts
@@ -1,18 +1,17 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateMerchantTable1729374646769 implements MigrationInterface {
+export class CreateCustomerTable1730145116687 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(` 
-      CREATE TABLE merchant (
+      CREATE TABLE customer (
           id INT PRIMARY KEY IDENTITY(1,1),
           username NVARCHAR(255) NOT NULL,
           password NVARCHAR(255) NOT NULL,
-          keypix NVARCHAR(255) NOT NULL          
       );`
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.query(`DROP TABLE merchant;`);
+    queryRunner.query(`DROP TABLE customer;`);
   }
 };
