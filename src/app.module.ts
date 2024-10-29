4f25e15091197a86381f54d4e3a3066696bd615d
ssssvio <r.savio@proton.me>
2024-10-29 09:39:24 -0300

    Refactor app module to add CustomerDataModule and MerchantModule dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-import { CustomerModule } from './modules/customer/customer.module';
-import { CustomerController } from './modules/customer/customer.controller';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './common/auth/auth.module';
 import { TypeOrmConfig } from './infra/config/typeorm.factory';
 import { MerchantModule } from './modules/merchant/merchant.module';
-import { AuthModule } from './common/auth/auth.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { CustomerController } from './modules/customer/customer.controller';
 
 @Module({
   imports: [
--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -1,12 +1,20 @@
-import { CustomerService } from './customer.service';
-
-
 import { Module } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { CustomerDataModule } from 'src/data/customer';
+import { CustomerController } from './customer.controller';
+import { ICustomerService } from './customer.service.interface';
+import { PostCustomerUseCase } from './usecases/post-customer.usecase';
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [
-        CustomerService,],
+  imports: [CustomerDataModule],
+  controllers: [CustomerController],
+  providers: [
+    CustomerService,
+    PostCustomerUseCase,
+    {
+      provide: ICustomerService,
+      useClass: CustomerService,
+    }
+  ],
 })
 export class CustomerModule { }
